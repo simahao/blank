eda9088c0c5a7fc9ea7543fb411ac51558fbb8b9
simahao <zhanghao@dce.com.cn>
2021-03-19 11:15:29 +0800

    init

---
+++ src/test/java/zh/ApplicationTest.java
@@ -0,0 +1,16 @@
+package zh;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Application")
+public class ApplicationTest {
+
+  @Test
+  @DisplayName("Pointless test")
+  void smokeTest() {
+    assertThat(true).isEqualTo(true);
+  }
+}
