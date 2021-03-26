8026e1123498029c752ea3b17e5f39527472f488
simahao <zhanghao@dce.com.cn>
2021-03-26 16:40:51 +0800

    log4j2

--- src/main/java/hz/Application.java
+++ src/main/java/hz/Application.java
@@ -1,7 +1,11 @@
 package hz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+      Logger log = LoggerFactory.getLogger(Application.class);
+      log.error("Hello World");
   }
 }
